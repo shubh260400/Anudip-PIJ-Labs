80b82eb53ae35110f0e2a910824e84a78c507a15
shubh260400 <patidarshubham222@gmail.com>
2025-05-03 14:45:46 +0530

    first commit

---
+++ Person.java
@@ -0,0 +1,11 @@
+//package com.anudip.learning;
+
+public class Person {
+    int age = 25;
+    int salary = 30000;
+    String name = "Rahul";
+
+    public static void main(String[] args) {
+        System.out.println("Test Successful");
+    }
+}
---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+    double width;
+    double height;
+
+    enum Color {
+        RED, GREEN, BLUE
+    }
+
+    Color boxColor;
+
+    public static void main(String[] args) {
+        System.out.println(Color.BLUE);
+    }
+}
